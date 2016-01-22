56a47aeaceb20fe0814750fb231ee32cdb271309
Niv Sardi <xaiki@evilgiggle.com>
2016-01-22 14:11:19 -0300

    fix webpack dev server
    Signed-off-by: Niv Sardi <xaiki@evilgiggle.com>

--- webpack.config.js
+++ webpack.config.js
@@ -5,7 +5,7 @@ var ExtractTextPlugin = require('extract-text-webpack-plugin');
 module.exports = {
   devtool: 'eval',
   entry: [
-    'webpack-dev-server/client?http://localhost:3000&',
+    'webpack-dev-server/client?http://localhost:3000',
     'webpack/hot/only-dev-server',
     path.join(__dirname, 'index.js')
   ],
