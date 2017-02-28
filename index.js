d04696806af3e251493536a9a553ff52506374fc
btzr-io <btzr.io@gmail.com>
2017-02-27 21:04:59 -0700

    Implement themes

--- index.js
+++ index.js
@@ -5,6 +5,7 @@ import { I18nextProvider} from 'react-i18next';
 import Component from 'btm_src';
 import i18n from './i18n';
 
+
 let testData = {}
 
 try {
@@ -13,4 +14,9 @@ try {
     console.error ('could not load test data')
 }
 
-render(<I18nextProvider i18n={ i18n }><Component {...testData}/></I18nextProvider>, document.getElementById('root'));
+
+const root = document.getElementById('root')
+root.className = "theme-dark";
+
+
+render(<I18nextProvider i18n={ i18n }><Component {...testData} /></I18nextProvider>, root);
