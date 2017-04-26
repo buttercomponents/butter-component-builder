a2462ebe77f377645618be557447c8ce5f02ee06
btzr-io <btzr.io@gmail.com>
2017-04-26 14:15:18 -0600

    Extract styles -> styles.css
     - Implement ExtractTextPlugin
    
    -Export and merge all styles chunk (css/stylus)
    
    - Fix externals root error

--- webpack.build.js
+++ webpack.build.js
@@ -1,5 +1,6 @@
 var path = require('path');
 var webpack = require('webpack');
+var ExtractStyle = require("extract-text-webpack-plugin");
 
 module.exports = {
     devtool: 'eval',
@@ -17,9 +18,10 @@ module.exports = {
     externals: {
         'react': { commonjs: 'react', commonjs2: 'react', amd: 'react', root: 'React' },
         'react-dom': { commonjs: 'react-dom', commonjs2: 'react-dom', amd: 'react-dom', root: 'ReactDOM' },
-        'react-addons-css-transition-group': { commonjs: 'react-addons-css-transition-group', commonjs2: 'react-addons-css-transition-group', amd: 'react-addons-css-transition-group', root: ['React','addons','CSSTransitionGroup']},
+        'react-addons-css-transition-group': { commonjs: 'react-addons-css-transition-group', commonjs2: 'react-addons-css-transition-group', amd: 'react-addons-css-transition-group', root: 'react-addons-css-transition-group'},
         'react-i18next':  { commonjs: 'react-i18next', commonjs2:'react-i18next', amd: 'react-i18next', root: 'react-i18next' }
     },
+    
     module: {
         rules: [{
             test: /\.jsx?$/,
@@ -33,23 +35,26 @@ module.exports = {
             ]
         }, {
             test: /\.(styl)$/,
-            use: [
-                'style-loader',
-                'css-loader?modules&importLoaders=1&localIdentName=[name]__[local]___[hash:base64:5]',
-                {
-                    loader: 'stylus-loader?paths=node_modules/bootstrap-stylus/stylus/',
-                    options: {
-                        use: [require('nib')()],
-                        import: ['~nib/index.styl', path.join(__dirname, 'styl/app.styl')]
-                    },
-                },
-            ],
+            loader: ExtractStyle.extract({
+                fallback:  "style-loader",
+                loader: [
+                    "css-loader?modules&importLoaders=1&localIdentName=[name]__[local]___[hash:base64:5]",
+                    {
+                        loader: 'stylus-loader?paths=node_modules/bootstrap-stylus/stylus/',
+                        options: {
+                            use: [require('nib')()],
+                            import: ['~nib/index.styl', path.join(__dirname, 'styl/app.styl')]
+                        }
+                    }
+                ]
+
+            })
         }, {
-            test: /\.(css)$/,
-            use: [
-                'style-loader',
-                'css-loader?importLoaders=1&localIdentName=[name]__[local]___[hash:base64:5]',
-            ]
+            test: /\.css$/,
+            loader: ExtractStyle.extract({
+                fallback:  "style-loader",
+                loader: "css-loader?importLoaders=1&localIdentName=[name]__[local]___[hash:base64:5]"
+            })
         }, {
             test: /\.(jpg|png|svg|woff2?|eot|ttf).*$/,
             loader: 'url-loader?limit=100000'
@@ -57,5 +62,8 @@ module.exports = {
             test:/bootstrap-sass[\/\\]assets[\/\\]javascripts[\/\\]/,
             loader: 'imports-loader?jQuery=jquery'
         }]
-    }
+    },
+    plugins: [
+        new ExtractStyle('styles.css')
+    ]
 };
