24844d96b6656b0c1ffea2a0ebfae9c9247cb470
Baltazar Gomez <btzr.io@gmail.com>
2017-05-08 22:29:03 -0600

    Update webpack.config.js

--- webpack.config.js
+++ webpack.config.js
@@ -12,7 +12,7 @@ module.exports = {
     entry: [
         'webpack-dev-server/client?http://localhost:3000',
         'webpack/hot/only-dev-server',
-        'webpack-material-design-icons',
+        'webpack-md-icons',
         'butter-theme-base',
         path.join(__dirname, 'index.js')
     ].concat(butter_themes),
