b01f22a54ff66d6b88be8455ab5e5cf5f5f83d48
Niv Sardi <xaiki@evilgiggle.com>
2018-04-04 22:56:10 -0300

    we have a build target, use it
    Signed-off-by: Niv Sardi <xaiki@evilgiggle.com>

--- update-package.json.js
+++ update-package.json.js
@@ -11,7 +11,7 @@ modifyPkgUp((pkg) => {
         scripts: {
             'prepublish': 'npm run build',
             'install': 'PATH=$PATH:../.bin webpack --config ../butter-component-builder/webpack/webpack.prod.config.js --progress --profile --colors',
-            'build': 'PATH=$PATH:../.bin webpack --config node_modules/butter-component-builder/webpack/webpack.prod.config.js --progress --profile --colors',
+            'build': 'PATH=$PATH:../.bin webpack --config node_modules/butter-component-builder/webpack/webpack.build.config.js --progress --profile --colors',
             'start': 'node node_modules/butter-component-builder/server/server.js',
             'lint': 'eslint src'
         }
