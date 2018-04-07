490e68c4c3fb2df44b737323a12b376f935985a8
Niv Sardi <xaiki@evilgiggle.com>
2018-04-07 19:07:35 -0300

    bcb-run: push arguments down, so you can pass arguments from yarn"
    Signed-off-by: Niv Sardi <xaiki@evilgiggle.com>

--- run.sh
+++ run.sh
@@ -1,11 +1,13 @@
 #!/bin/sh
+arg=$1; shift
 
-case $1 in
-        install) PATH=$PATH:../.bin webpack --config ../butter-component-builder/webpack/webpack.prod.config.js --progress --profile --colors;;
-        build) PATH=$PATH:../.bin webpack --config node_modules/butter-component-builder/webpack/webpack.build.config.js --progress --profile --colors;;
-        start) webpack-dev-server --config node_modules/butter-component-builder/webpack/webpack.dev.config.js --progress --profile --colors --open;;
-        lint) eslint src;;
-        update) node node_modules/butter-component-builder/update-package.json.js;;
+case $arg in
+        install) PATH=$PATH:../.bin webpack --config ../butter-component-builder/webpack/webpack.prod.config.js --progress --profile --colors $@;;
+        build) PATH=$PATH:../.bin webpack --config node_modules/butter-component-builder/webpack/webpack.build.config.js --progress --profile --colors $@;;
+        start) webpack-dev-server --config node_modules/butter-component-builder/webpack/webpack.dev.config.js --progress --profile --colors $@;;
+        open) $0 start --open $@;;
+        lint) eslint src $@;;
+        update) node node_modules/butter-component-builder/update-package.json.js $@;;
         *) echo "
 usage $0 [install,build,start,lint]
       install:    run as a hook when installing package
