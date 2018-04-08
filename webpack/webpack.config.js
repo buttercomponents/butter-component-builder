24d90abbdfef2f430bf473691faa57eb5b3ac72d
Niv Sardi <xaiki@evilgiggle.com>
2018-04-08 01:55:18 -0300

    webpack: finally find a valid exclude pattern
    Signed-off-by: Niv Sardi <xaiki@evilgiggle.com>

--- webpack/webpack.config.js
+++ webpack/webpack.config.js
@@ -19,11 +19,7 @@ butter_components.add('butter-base-components')
 
 const jsxConfig = {
   test: /\.jsx?$/,
-  /* exclude: /node_modules/,
-   * include: [
-   *   Array.of(butter_components).map(c => new RegExp(c)),
-   *   path.resolve('./src/')
-   * ],*/
+  exclude: /node_modules\/[^butter]/,
   loader: 'babel-loader',
   options: {
     cacheDirectory: process.env.NODE_ENV === 'development',
