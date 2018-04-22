1bac71612fb830b6ef1685af425d4d91954e45ea
Niv Sardi <xaiki@evilgiggle.com>
2018-04-22 09:53:24 -0300

    export to UMD bundles
    Signed-off-by: Niv Sardi <xaiki@evilgiggle.com>

--- webpack/webpack.build.config.js
+++ webpack/webpack.build.config.js
@@ -1,11 +1,16 @@
 const config = require('./webpack.prod.config');
 const path = require('path');
 
+const packageJSON = require(path.join(process.cwd(), 'package.json'));
+
 module.exports = Object.assign(config, {
   entry: Object.assign(config.entry, {
     index: path.join(process.env.PWD||process.cwd(), 'index.js'),
   }),
   output:  {
+    library: packageJSON.name,
+    libraryTarget: 'umd',
+    auxiliaryComment: packageJSON.description,
     path: path.join(process.env.PWD||process.cwd(), 'dist'),
     filename: '[name].js',
     publicPath: '/',
